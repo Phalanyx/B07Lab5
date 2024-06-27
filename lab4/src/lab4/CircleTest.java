6964747ec42402ee0e4bdddd7a6ff39fa4282451
oscar <oscar.pang050906@gmail.com>
2024-06-27 11:34:07 -0400

    test: added tests for the circle class

---
+++ lab4/src/lab4/CircleTest.java
@@ -0,0 +1,64 @@
+package lab4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+
+public class CircleTest {
+
+    @Test
+    void testDiameter() {
+        Point p = new Point(0, 0);
+        Circle c = new Circle(p, 5);
+        assertEquals(c.diamater(), 10);
+    }
+
+    @Test
+    void testPerimeter() {
+        Point p = new Point(1, 2);
+        Circle c = new Circle(p, 5);
+        assertEquals(c.perimeter(), 31.41593, 1e-5);
+    }
+
+    @Test
+    void testArea() {
+        Point p = new Point(3, 4);
+        Circle c = new Circle(p, 5);
+        assertEquals(c.area(), 78.53982, 1e-5);
+    }
+
+    @Test
+    void testIsOnCircle1() {
+        Point p = new Point(0, 0);
+        Circle c = new Circle(p, 10);
+        Point p1 = new Point(0, 10);
+        assertTrue(c.isOnCircle(p1));
+    }
+
+    @Test
+    void testIsOnCircle2() {
+        Point p = new Point(1, 1);
+        Circle c = new Circle(p, 10);
+        Point p1 = new Point(1, 5);
+        assertFalse(c.isOnCircle(p1));
+    }
+
+    @Test
+    void testIsEquals1() {
+        Point p = new Point(1, 1);
+        Circle c1 = new Circle(p, 10);
+        Circle c2 = new Circle(p, 10);
+        assertTrue(c1.equals(c2));
+    }
+
+    @Test
+    void testIsEquals2() {
+        Point p = new Point(1, 1);
+        Circle c1 = new Circle(p, 10);
+        Circle c2 = new Circle(p, 5);
+        assertFalse(c1.equals(c2));
+    }
+
+}
